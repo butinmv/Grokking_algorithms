661dbeca8e5e72e49246edcf9de6d49a7cd63ee7
Maxim Butin <butinmv9722@gmail.com>
2020-08-29 13:30:20 +0700

    Selection Sort project init

---
+++ Selection Sort/Selection Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Selection Sort
+//
+//  Created by Maxim Butin on 29.08.2020.
+//  Copyright © 2020 Maxim Butin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

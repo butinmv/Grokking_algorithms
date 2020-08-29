03ce57059e4b9841c7637de6ea2cfebd92be75a1
Maxim Butin <butinmv9722@gmail.com>
2020-08-29 09:11:58 +0700

    Creating and linking nodes

---
+++ Linked List/Linked List/Node.swift
@@ -0,0 +1,31 @@
+//
+//  Node.swift
+//  Linked List
+//
+//  Created by Maxim Butin on 29.08.2020.
+//  Copyright © 2020 Maxim Butin. All rights reserved.
+//
+
+import Foundation
+
+public class Node<Value> {
+    
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
--- Linked List/Linked List/main.swift
+++ Linked List/Linked List/main.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Creating and linking nodes
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
 
+node1.next = node2
+node2.next = node3
+
+print(node1)

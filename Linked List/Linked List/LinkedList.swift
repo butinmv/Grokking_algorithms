e5c24f366a2e4d94cc9f7d505db05f72cafc8b81
Maxim Butin <butinmv9722@gmail.com>
2020-08-29 09:15:44 +0700

    Creating Linked List struct

---
+++ Linked List/Linked List/LinkedList.swift
@@ -0,0 +1,31 @@
+//
+//  LinkedList.swift
+//  Linked List
+//
+//  Created by Maxim Butin on 29.08.2020.
+//  Copyright © 2020 Maxim Butin. All rights reserved.
+//
+
+import Foundation
+
+public struct LinkedList<Value> {
+    
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    
+    public var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head) 
+    }
+}

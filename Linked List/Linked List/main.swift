b4da588705ec7bbf35808324d0913ae53b3c90a8
Maxim Butin <butinmv9722@gmail.com>
2020-08-29 09:03:52 +0700

    Init Linked List project

---
+++ Linked List/Linked List/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Linked List
+//
+//  Created by Maxim Butin on 29.08.2020.
+//  Copyright © 2020 Maxim Butin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
